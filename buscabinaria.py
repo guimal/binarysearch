85d8249d5dceab873c7066319aed123500746a18
guimal <31482760+guimal@users.noreply.github.com>
2017-08-31 21:49:39 -0300

    Update buscabinaria.py

--- buscabinaria.py
+++ buscabinaria.py
@@ -1,1 +1,12 @@
-
+def buscabinaria(vet, num):
+	esquerda, direita, tentativa = 0, len(vet), 1
+	while 1:
+		meio = (esquerda + direita) // 2
+		aux_num = vet[meio]
+		if num == aux_num:
+			return tentativa
+		elif num > aux_num:
+			esquerda = meio
+		else:
+			direita = meio
+tentativa += 1
